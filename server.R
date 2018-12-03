e9d35ac698df78f2cbf783325c8f4eb60b00c849
WayneLi1063 <weileli@uw.edu>
2018-12-02 18:16:24 -0800

    Functional second graph!

--- server.R
+++ server.R
@@ -44,19 +44,18 @@ shinyServer(function(input, output) {
   output$select_genre <- renderUI({
     selectInput("genre",
                 label = "Genre",
-                choices = genre_datas$name)
+                choices = c("All", genre_datas$name))
   })
   output$select_theme <- renderUI({
     selectInput("theme",
                 label = "Theme",
-                choices = theme_datas$name)
+                choices = c("All", theme_datas$name))
   })
   output$select_franchise <- renderUI({
     selectInput("franchise",
                 label = "Franchise",
-                choices = collection_datas$name,
+                choices = c("All", collection_datas$name),
                 selectize = FALSE,
-                multiple = TRUE,
                 size = 20)
   })
   output$select_year_wayne <- renderUI({
@@ -67,23 +66,38 @@ shinyServer(function(input, output) {
                 value = c(1971, 2018))
   })
   output$base_game <- renderUI({
-    checkboxInput("base", label = "Base games only", value = TRUE)
+    checkboxInput("base", label = "Base games only", value = FALSE)
   })
   output$lineplot <- renderPlotly({
-    selected_genre <- genre_datas %>% filter(name == input$genre[1])
-    gamelist <- unlist(selected_genre$games, use.names = FALSE)
+    game_data <- game_datas_all
+    if (input$genre[1] != "All") {
+      selected_genre <- genre_datas %>% filter(name == input$genre[1])
+      gamelist <- unlist(selected_genre$games, use.names = FALSE)
+      game_data <- game_data %>% filter(id %in% gamelist)
+    }
+    if (input$theme[1] != "All") {
+      selected_theme <- theme_datas %>% filter(name == input$theme[1])
+      gamelist <- unlist(selected_theme$games, use.names = FALSE)
+      game_data <- game_data %>% filter(id %in% gamelist)
+    }
+    if (input$franchise[1] != "All") {
+      selected_franchise <- collection_datas %>% filter(name == input$franchise[1])
+      gamelist <- unlist(selected_franchise$games, use.names = FALSE)
+      game_data <- game_data %>% filter(id %in% gamelist)
+    }
     start_year <- as.Date(paste0(input$year[1], "-01-01"))
     end_year <- as.Date(paste0(input$year[2], "-12-31"))
-    game_data <- game_datas_all %>%
-      filter(start_year <= first_release_date & end_year >= first_release_date) %>%
-      filter(id %in% gamelist)
-    if (!input$base[1]) {
-      game_data <- game_data %>% filter(is.na(version_parent))
+    game_data <- game_data %>%
+      filter(start_year <= first_release_date & end_year >= first_release_date)
+    if (input$base[1]) {
+      game_data <- game_data %>% filter(is.na(game))
     }
     game_data <- game_data %>% select(name, first_release_date, total_rating) %>%
       arrange(first_release_date)
-    plot <- plot_ly(game_data, x = ~first_release_date, y = ~total_rating,
-                   type = 'scatter', mode = 'lines+markers', text = ~name)
+    if (nrow(game_data) >= 0) {
+      plot <- plot_ly(game_data, x = ~first_release_date, y = ~total_rating,
+                      type = 'scatter', mode = 'lines+markers', text = ~name)
+    }
   })
   output$select_year <- renderUI({
     return(selectInput("year_selection", "Select Release Year", choices=generate_unique_release_year()))
